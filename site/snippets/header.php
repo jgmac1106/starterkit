ea8293bb0f867d47e96e40ec86459d78f8a79a3c
Greg McVerry <jgregmcverry@gmail.com>
2019-05-25 17:10:00 -0400

    Update header.php

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,25 +1,74 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <title>INTERTEXTrEVOLUTION</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/reset.css">
+    
+    <link rel="stylesheet" href="/styles.css">
+     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-9ralMzdK1QYsk4yBY680hmsb4/hJ98xK3w0TIaJ3ll4POWpWUYaA2bRjGGujGT8w" crossorigin="anonymous">
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
-
-  <title><?= $site->title() ?> | <?= $page->title() ?></title>
-
-  <?= css(['assets/css/index.css', '@auto']) ?>
-
-</head>
-<body>
-
-  <div class="page">
-    <header class="header">
-      <a class="logo" href="<?= $site->url() ?>"><?= $site->title() ?></a>
-
-      <nav id="menu" class="menu">
-        <?php foreach ($site->children()->listed() as $item): ?>
-        <?= $item->title()->link() ?>
-        <?php endforeach ?>
-      </nav>
+    <script src="/script.js" defer></script>
+    <link rel="webmention" href="https://webmention.io/jgmac1106homepage.glitch.me/webmention" />
+<link rel="pingback" href="https://webmention.io/jgmac1106homepage.glitch.me/xmlrpc" />
+    <link rel="authorization_endpoint" href="https://indieauth.com/auth">
+<link rel="token_endpoint" href="https://tokens.indieauth.com/token">
+    <link rel="webmention" href="https://webmention.io/jgregorymcverry.com/webmention" />
+<link rel="pingback" href="https://webmention.io/jgregorymcverry.com/xmlrpc" />
+  </head>
+  <body>
+    <header>
+       <a href="index.html">
+   <div class="leading">
+     <h1>
+       INTERTEXTrEVOLUTION
+     </h1>
+     <h2>
+       Make.Hack.Play.Learn
+     </h2>
+      </div>
+     </a>  
+      <div class="Navbar">
+          
+          <a href="/index.html"><i class="fas fa-home-heart"></i></a>
+          <div class="Navbar__Link Navbar__Link-toggle">
+      <i class="fas fa-bars"></i>
+    </div>
+      </div>
+  <nav class="Navbar__Items">
+    <div class="Navbar__Link">
+    
+    <a href="/aboutme.html" >About</a>
+    </div>
+    <div class="Navbar__Link">
+    <a href="/articles.html" >Articles</a>
+    </div>
+        <div class="Navbar__Link">
+    <a href="https://quickthoughts.jgregorymcverry.com.html" >Blog</a>
+    </div>
+     
+      <div class="Navbar__Link">
+    <a href="/contact.html">Contact</a>
+    </div>
+     <div class="Navbar__Link">
+    <a href="/resume.html" class="navLinks">Vita</a>
+    </div>
+      <div class="Navbar__Link">
+    <a href="https://consulting.jgregorymcverry.com/" class="navLinks">Consulting</a>
+    </div>
+        <div class="Navbar__Link">
+    <a href="/2toPonder.html" class="navLinks">Podcasts</a>
+    </div>
+        <div class="Navbar__Link">
+    <a href="/following.html" class="navLinks">Following</a>
+    </div>
+            <div class="Navbar__Link">
+    <a href="/annotations.html" class="navLinks">Annotations</a>
+    </div>
+            <div class="Navbar__Link">
+    <a href="/imagecredits.html" class="navLinks">Img Credits</a>
+    </div>
+  </nav>
     </header>
-
